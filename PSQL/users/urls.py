5eba4010a7b919fb2faf6da96c234ec24ce49b83
Albinoni <albinoni1423@gmail.com>
2024-10-16 03:49:20 -0300

    CreateUser added

--- PSQL/users/urls.py
+++ PSQL/users/urls.py
@@ -3,4 +3,5 @@ from . import views
 
 urlpatterns = [
     path('allusers/',  views.GetUsers),
+    path('create-user/',  views.CreateUser),
 ]
--- PSQL/users/views.py
+++ PSQL/users/views.py
@@ -1,18 +1,61 @@
-from django.shortcuts import render
+from django.views.decorators.csrf import csrf_exempt
 from django.http import JsonResponse
-from . import models
+from .models import User
+import json
 
-# Create your views here.
+# Get
 
 
 def GetUsers(request):
-    all_users = models.User.objects.all()
+    if request.method == "GET":
+        # Fetch all users from the database
+        all_users = User.objects.all()
 
-    user_data = [{"id": user.id, "username": user.username}
-                 for user in all_users]
+        # Prepare a list of user data to return
+        user_data = [{"id": user.id, "username": user.username} for user in all_users]
 
-    data = {
-        "users": user_data
-    }
+        # Prepare the response data
+        data = {
+            "users": user_data,
+            "total_users": all_users.count()  # Include the total count of users
+        }
 
-    return JsonResponse(data)
+        # Return a JsonResponse with the data
+        return JsonResponse(data, status=200)
+    
+    # If the request method is not GET, return an error response
+    return JsonResponse({"error": "Only GET method is allowed"}, status=405)
+
+
+# Post
+
+@csrf_exempt
+def CreateUser(request):
+    if request.method == "POST":
+        try:
+            data = json.loads(request.body)  # Parse the request body
+            username = data.get("username")
+            password = data.get("password")
+
+            if not username or not password:
+                return JsonResponse({"error": "Missing fields"}, status=400)
+
+            # Check if Username Taken
+
+            all_users = User.objects.all()
+
+            # Create a new user
+            user = User.objects.create(
+                username=username, password=password)
+
+            return JsonResponse({
+                "message": "User created successfully",
+                "user": {
+                    "id": user.id,
+                    "username": user.username,
+                }
+            }, status=201)
+        except json.JSONDecodeError:
+            return JsonResponse({"error": "Invalid JSON data"}, status=400)
+
+    return JsonResponse({"error": "Only POST method is allowed"}, status=405)
